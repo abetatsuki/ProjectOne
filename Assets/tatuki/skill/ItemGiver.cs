d7bfe00f9bb29fa9acbe87e5d5bf3d039aa00165
kenkatou354 <kenkatou354@gmail.com>
2025-10-25 16:03:18 +0900

    [Add]追加したスキルが別radiusを回るように

--- Assets/tatuki/skill/ItemGiver.cs
+++ Assets/tatuki/skill/ItemGiver.cs
@@ -1,14 +1,20 @@
 using System;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.InputSystem.iOS;
 
 public class ItemGiver : MonoBehaviour
 {
     [SerializeField] private OrbitManager orbitManager;
     [SerializeField] private GameObject itemPrefab;
-   
+    private int count = 3;
 
-    
-    
-    public void Add() =>orbitManager.AddRing(itemPrefab, 3, 4f, 60f);
+
+
+
+    public void Add()
+    {
+        orbitManager.AddRing(itemPrefab, count, count, 60f);
+        count += 3;
+    }
 }
